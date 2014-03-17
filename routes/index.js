b76a1e8f402ab88a0fa61c21b7139d04ee3adb31
jonmiles <jonathan.miles@interlinksoftware.com>
2014-03-17 10:41:56 +0000

    Updated to push event data from server routing to view on page load

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,19 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+	var eventData = [{
+			title: 'event3',
+			start: '2014-03-03 12:30:00',
+			end: '2014-03-03 16:30:00',
+			allDay: false
+		},
+		{
+			title: 'event4',
+			start: '2014-03-04 12:30:00',
+			end: '2014-03-04 16:30:00',
+			allDay: false
+		}
+	];
+	res.render('index', { data: eventData });
 };
